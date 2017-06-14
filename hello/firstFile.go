eaf30288ad91468c42437e3a88dacecb7042336d
Matt Smith <mjsmith92@outlook.com>
2017-06-14 14:31:28 -0400

    hello world

---
+++ hello/firstFile.go
@@ -0,0 +1,7 @@
+package main //this is the entry point all other packages must match folder name
+
+import "fmt"
+
+func main() { //package main must have func main()
+	fmt.Println("Hello World!!!!") //Println is capital in fmt which makes it 'public' and available here
+}
