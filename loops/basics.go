cb6442d3fcfe4248cb38338fb5720097b8a97f1f
matt smith <mjsmith92@outlook.com>
2017-06-28 21:42:28 -0400

    The rest of loops

--- loops/basics.go
+++ loops/basics.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	loopOverString()
+	evensTo1000()
 }
 
 func countTo100() {
@@ -22,8 +22,21 @@ func countTo100() {
 }
 
 func loopOverString() {
+	//the value is a rune which is an alias for int32 used when it is representing a UTF-8 character
+
 	myPhrase := "What is a good phrase?"
 	for key, value := range myPhrase {
 		fmt.Println(key, " - ", string(value))
+	}
+
+	//for i := 0; i < len(myPhrase); i++ {
+	//	fmt.Println(i, " - ", string(myPhrase[i]))
+
+	//}
+}
+
+func evensTo1000() {
+	for i := 2; i <= 1000; i += 2 {
+		fmt.Println(i)
 	}
 }
