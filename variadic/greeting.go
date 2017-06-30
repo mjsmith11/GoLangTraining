316e9a0d9987c554925da00ac2d3866bd48ead67
matt smith <mjsmith92@outlook.com>
2017-06-29 21:29:53 -0400

    Variadic Param Greeting

---
+++ variadic/greeting.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Greeting(prefix string, who ...string) {
+	fmt.Println(prefix)
+	for _, value := range who {
+		fmt.Println(value)
+	}
+}
+
+func main() {
+	Greeting("nobody")
+	Greeting("hello: ", "Joe", "Anna", "Eileen")
+	s := []string{"James", "Jasmine"}
+	Greeting("goodbye:", s...)
+}
