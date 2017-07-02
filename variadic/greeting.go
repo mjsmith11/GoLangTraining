09ffe383abd2aefcde77eb97589e1f85ae799ef1
matt smith <mjsmith92@outlook.com>
2017-07-02 17:19:27 -0400

    slice vs variadic params

--- variadic/greeting.go
+++ variadic/greeting.go
@@ -11,9 +11,17 @@ func Greeting(prefix string, who ...string) {
 	}
 }
 
+func GreetingS(prefix string, who []string) {
+	fmt.Println(prefix)
+	for _, value := range who {
+		fmt.Println(value)
+	}
+}
+
 func main() {
 	Greeting("nobody")
 	Greeting("hello: ", "Joe", "Anna", "Eileen")
 	s := []string{"James", "Jasmine"}
 	Greeting("goodbye:", s...)
+	GreetingS("Goodbye", s)
 }
