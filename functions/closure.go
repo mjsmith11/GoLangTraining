a6705a46218f87f9117210c8e0d0944ea2e4c201
matt smith <mjsmith92@outlook.com>
2017-07-06 22:39:22 -0400

    Closure

--- functions/closure.go
+++ functions/closure.go
@@ -5,7 +5,7 @@ import "fmt"
 //closure - one thing enclosing another thing. limit scope
 func main() {
 	x := 0
-	increment := func() int { //increment is enclosed by main as is x. Thus increment can access x
+	increment := func() int { //increment is enclosed by main as is x. Thus increment can access x without passing it in
 		x++
 		return x
 	}
