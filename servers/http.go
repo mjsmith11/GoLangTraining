6db8d5480830f169f39f5aecd09776e98534e565
matt smith <mjsmith92@outlook.com>
2017-07-20 22:32:57 -0400

    Simple http server that extracts http verb

---
+++ servers/http.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+	i := 0
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+
+		if i == 0 {
+			method := strings.Fields(ln)[0] //divided by whitespace
+			fmt.Println("WE PRINTED THIS - METHOD : ", method)
+		}
+		i++
+	}
+}
+
+func main() {
+	server, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer server.Close()
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		go handleConn(conn) // what does go do?? threading?
+	}
+}
