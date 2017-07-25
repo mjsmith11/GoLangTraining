a9bd93c002ac376c06a7e5ec689cd7489b94f552
matt smith <mjsmith92@outlook.com>
2017-07-24 21:47:44 -0400

    Playing with cookies

---
+++ cookies/cookie.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func getAndSetCookie(res http.ResponseWriter, req *http.Request) {
+	cookie, _ := req.Cookie("my-cookie")
+	fmt.Println(cookie)
+	io.WriteString(res, cookie.Value)
+
+	http.SetCookie(res, &http.Cookie{
+		Name:  "my-cookie",
+		Value: "COOKIE MONSTER",
+	})
+}
+
+func main() {
+	http.HandleFunc("/", getAndSetCookie)
+	http.ListenAndServe(":9000", nil)
+}
